46bcdbd90f4b4722321e91af1e355f1966ed98dc
Zholaman03 <sikhzholaman@gmail.com>
2024-08-19 15:43:48 +0500

    changed size videos

--- resources/views/info/info.blade.php
+++ resources/views/info/info.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.app')
 
 @section('title', 'Пользовательская инструкция')
+@section('style') 
+<style>
+    .video-container {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-bottom: 56.25%; /* 16:9 пропорциясы */
+    }
 
+    .video-container iframe, 
+    .video-container video {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+   @media (max-width: 768px) {
+    .video-container {
+        width: 100%;
+        height: auto;
+    }
+    
+    .video-container video{
+        width: 100%;
+        height: auto;
+    }
+}
+
+</style>
+@endsection
 @section('content')
 <div class="container">
     <h1>Пользовательская инструкция</h1>
@@ -20,12 +50,14 @@
                               (<a class="text-decoration-none" href="{{route('login')}}">Вход</a>). 
                               После успешной авторизации вы сможете создать свой текст. Обратите внимание, 
                               что ваш текст будет опубликован только после проверки администратором.</p>
-                        <video class="card-video w-75" controls>
-                            <source src="{{ asset('storage/videos/create.mp4') }}" type="video/mp4">
-                            Ваш браузер не поддерживает видео тег.
-                        </video>
+                              <div class="video-container">
+                                <video controls>
+                                    <source src="{{ asset('storage/videos/create.mp4') }}" type="video/mp4">
+                                    Ваш браузер не поддерживает видео тег.
+                                </video>
+                              </div>
+                        </div>
                     </div>
-                </div>
             </div>
             
             <!-- Удалить пост -->
@@ -37,10 +69,12 @@
                     <div class="card-body">
                         <p class="card-text">Если вы хотите удалить свой текст, вам тоже необходимо пройти авторизацию (<a class="text-decoration-none" href="{{route('login')}}">Вход</a>). 
                         </p>
-                        <video class="card-video w-75" controls>
-                            <source src="{{ asset('storage/videos/delete.mp4') }}" type="video/mp4">
-                            Ваш браузер не поддерживает видео тег.
-                        </video>
+                            <div class="video-container">
+                                <video controls>
+                                    <source src="{{ asset('storage/videos/delete.mp4') }}" type="video/mp4">
+                                    Ваш браузер не поддерживает видео тег.
+                                </video>
+                              </div>
                     </div>
                 </div>
             </div>
@@ -54,10 +88,12 @@
                     <div class="card-body">
                         <p class="card-text">Если вы хотите изменить свой текст, вам необходимо пройти авторизацию (<a class="text-decoration-none" href="{{route('login')}}">Вход</a>),  
                         и изменения будут опубликованы только после проверки администратором.</p>
-                        <video class="card-video w-75" controls>
-                            <source src="{{ asset('storage/videos/edit.mp4') }}" type="video/mp4">
-                            Ваш браузер не поддерживает видео тег.
-                        </video>
+                        <div class="video-container">
+                                <video controls>
+                                    <source src="{{ asset('storage/videos/edit.mp4') }}" type="video/mp4">
+                                    Ваш браузер не поддерживает видео тег.
+                                </video>
+                              </div>
                     </div>
                 </div>
             </div>
